95d654cc28c07cc8dc71a7745c4c8e280a24eaa9
Adeleye Abdulbasit Damilola <adeleyeabdulbasitdamilola@gmail.com>
2024-08-04 15:08:41 +0100

    fix: search and UI deficiency

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import { Box } from '@mui/material'
-import NavBar from './components/NavBar'
-import Home from './pages/Home'
-import ExerciseDetail from './pages/ExerciseDetail'
-import Footer from './components/Footer'
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
 
-
-function App() {
-
-  return (
-    <Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/exercise" element={<ExerciseDetail />} />
-      </Routes>
-      <Footer />
-
-    </Box>
-  )
-}
-
-export default App
+export default App;
